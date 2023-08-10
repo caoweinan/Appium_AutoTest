55642e104b4d01ae428475e368638c1189d45409
caoweinan <caoweinan@bailian.ai>
2023-08-10 10:05:17 +0800

    修改host名称，实现远程调用appium

--- run.py
+++ run.py
@@ -19,14 +19,17 @@ if __name__ == '__main__':
 
     # 自定义参数
     # 启动appium需要信息，--cmdopt字典
-    cmdopt = {"host": "127.0.0.1",
+
+    # jenkins是用Mac本机地址"host":"10.211.55.2"，本地用"host": "127.0.0.1"，外面生成报告也要注释掉，在里面加生成报告pytest.main([f"--cmdopt={cmdopt}","--alluredir",report_path])
+
+    cmdopt = {"host": "10.211.55.2",
               "port": "4723",
               "bpport": "4724",
               "udid": "WQVNW18305004438",
               "systemPort":8200}
-    pytest.main([f"--cmdopt={cmdopt}"])     #加f表示{cmdopt}是个参数
+    # pytest.main([f"--cmdopt={cmdopt}"])     #加f表示{cmdopt}是个参数
+    pytest.main([f"--cmdopt={cmdopt}","--alluredir",report_path])     #加f表示{cmdopt}是个参数
     time.sleep(2)
-    allure_generate(report_path, report_html)
+    # allure_generate(report_path, report_html)
     # time.sleep(3)
     # send_mail(content="测试完成，请查看测试报告")
-
--- testcase/conftest.py
+++ testcase/conftest.py
@@ -14,7 +14,34 @@ def pytest_addoption(parser):
 def cmdopt(request):
     return request.config.getoption("--cmdopt")
 
+"""
+本地运行
+"""
+# # 调用命令使用
+# @pytest.fixture(scope="session")
+# def start_appium_desired(cmdopt):
+#     opt = eval(cmdopt)
+#     # {'host': '127.0.0.1', 'port': '4723', 'bpport': '4724', 'udid': None}
+#     host = opt["host"]
+#     port = opt["port"]
+#     bpport = opt["bpport"]
+#     udid = opt["udid"]
+#     systemPort = opt["systemPort"]
+#     # print(opt)
+#     driver = None
+#     if udid in get_devices():
+#         appium_start(host, port, bpport, udid)
+#         time.sleep(5)
+#         if not check_port():
+#             driver = appium_desired_caps(host, port,systemPort)
+#     # return driver
+#     yield driver
+#     driver.quit()
 
+
+"""
+Jenkins运行start_appium_desired
+"""
 # 调用命令使用
 @pytest.fixture(scope="session")
 def start_appium_desired(cmdopt):
@@ -27,11 +54,13 @@ def start_appium_desired(cmdopt):
     systemPort = opt["systemPort"]
     # print(opt)
     driver = None
-    if udid in get_devices():
-        appium_start(host, port, bpport, udid)
-        time.sleep(5)
-        if not check_port():
-            driver = appium_desired_caps(host, port,systemPort)
+    # if udid in get_devices():
+    #     appium_start(host, port, bpport, udid)
+    #     time.sleep(5)
+    #     if not check_port():
+    #         driver = appium_desired_caps(host, port,systemPort)
+    if not check_port(host,port):
+        driver = appium_desired_caps(host, port,systemPort)
     # return driver
     yield driver
     driver.quit()
